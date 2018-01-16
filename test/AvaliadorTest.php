2ca9da548778738e18210005f82f2366f9ea0b7c
André Krebs <andre.krebssilva@gmail.com>
2018-01-16 14:16:38 -0200

    Mais testes

--- test/AvaliadorTest.php
+++ test/AvaliadorTest.php
@@ -29,8 +29,8 @@ class AvaliadorTest extends TestCase {
         $maiorEsperado = 400;
         $menorEsperado = 250;
 
-        $this->assertEquals($leiloeiro->getMaiorLance(),$maiorEsperado);
-        $this->assertEquals($leiloeiro->getMenorLance(),$menorEsperado);
+        $this->assertEquals($maiorEsperado, $leiloeiro->getMaiorLance());
+        $this->assertEquals($menorEsperado, $leiloeiro->getMenorLance());
 
 
     }
@@ -61,4 +61,62 @@ class AvaliadorTest extends TestCase {
         $this->assertEquals($mediaEsperada, $leiloeiro->getMediaLances(), 0.0001);
         
     }
+    
+    public function testUnicoLanceValorMaiorEMenor() {
+        
+        $joao = new Usuario("João");
+        
+        $leilao = new Leilao("Playstation 4");
+        
+        $leilao->propoe(new Lance($joao, 200));
+        
+        $leiloeiro = new Avaliador();
+        $leiloeiro->avalia($leilao);
+        
+        $this->assertEquals(200, $leiloeiro->getMenorLance());
+        $this->assertEquals(200, $leiloeiro->getMaiorLance());
+    }
+    
+    public function testLancesValoresForaDeOrdem() {
+        $joao = new Usuario("Joao");
+        $renan = new Usuario("Renan");
+        $felipe = new Usuario("Felipe");
+
+        $leilao = new Leilao("Playstation 4");
+
+        $leilao->propoe(new Lance($joao,200));
+        $leilao->propoe(new Lance($renan,450));
+        $leilao->propoe(new Lance($felipe,120));
+        $leilao->propoe(new Lance($joao,700));
+        $leilao->propoe(new Lance($renan,630));
+        $leilao->propoe(new Lance($felipe,230));
+        
+        $leiloeiro = new Avaliador();
+        
+        $leiloeiro->avalia($leilao);
+        
+        $this->assertEquals(120, $leiloeiro->getMenorLance());
+        $this->assertEquals(700, $leiloeiro->getMaiorLance());
+    }
+    
+    public function testLancesValorDecrescente() {
+        $joao = new Usuario("Joao");
+        $renan = new Usuario("Renan");
+        $felipe = new Usuario("Felipe");
+
+        $leilao = new Leilao("Playstation 4");
+
+        $leilao->propoe(new Lance($joao,400));
+        $leilao->propoe(new Lance($renan,300));
+        $leilao->propoe(new Lance($felipe,200));
+        $leilao->propoe(new Lance($joao,100));
+        
+        $leiloeiro = new Avaliador();
+        
+        $leiloeiro->avalia($leilao);
+        
+        $this->assertEquals(100, $leiloeiro->getMenorLance());
+        $this->assertEquals(400, $leiloeiro->getMaiorLance());
+    }
+    
 }
